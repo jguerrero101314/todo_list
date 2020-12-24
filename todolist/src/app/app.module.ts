b5a6f7486b1a50b3b386e45e8f876af9d3feffae
Joel Guerrero <joelg1014@hotmail.com>
2020-12-23 21:50:56 -0800

    formulario para las notas

--- todolist/src/app/app.module.ts
+++ todolist/src/app/app.module.ts
@@ -5,25 +5,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // components
 
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
+
+
 
 // environments
 
 import { environment } from '../environments/environment';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+
+
+
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
+  declarations: [
+    AppComponent,
+    TodoListComponent
+
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
+ 
 })
 export class AppModule {}
