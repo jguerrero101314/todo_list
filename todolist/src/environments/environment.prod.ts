3cab70973dd1b4650526ab4fad9baf544b370196
Joel Guerrero <joelg1014@hotmail.com>
2020-12-23 18:45:28 -0800

    creacion del proyecto

---
+++ todolist/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyCzUpMkoP3mj1jAkpdJmcomXnBlG8fLUHA",
+    authDomain: "todolist-51830.firebaseapp.com",
+    projectId: "todolist-51830",
+    storageBucket: "todolist-51830.appspot.com",
+    messagingSenderId: "308669491137",
+    appId: "1:308669491137:web:ebc1e4675409375815f140",
+    measurementId: "G-KNL5GFF5M8"
+  }
+};
---
+++ todolist/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyCzUpMkoP3mj1jAkpdJmcomXnBlG8fLUHA",
+    authDomain: "todolist-51830.firebaseapp.com",
+    projectId: "todolist-51830",
+    storageBucket: "todolist-51830.appspot.com",
+    messagingSenderId: "308669491137",
+    appId: "1:308669491137:web:ebc1e4675409375815f140",
+    measurementId: "G-KNL5GFF5M8"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
